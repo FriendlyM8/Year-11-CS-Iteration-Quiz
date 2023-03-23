f317916a662d14d486bf51a6f3e8a4c25e171419
Alden <aldenc2023@student.cis.edu.hk>
2023-03-23 10:09:08 +0800

    Final

--- src/Quiz.java
+++ src/Quiz.java
@@ -3,6 +3,18 @@ public class Quiz {
     /** 1. Write a method called numberOfVowels that returns the number of vowels in a string.
      Consider a, e, i, o, u as vowels. **/
 
+    public static int numberOfVowels(String str) {
+        String vowels = "aeiouAEIOU";
+
+        int numOfVow = 0;
+
+        for(int i = 0; i < str.length();i++){
+            if (vowels.indexOf(str.charAt(i)) != -1) {
+                numOfVow++;
+            }
+        }
+        return numOfVow;
+    }
 
     /** 2. Write a method called notDivisibleBy235 that finds the number of natural numbers not exceeding N
      and not divisible by any of the numbers [2, 3, 5].
@@ -15,6 +27,16 @@ public class Quiz {
      5 - divides integer by 5
      Answer: 1 (only one number doesn't divide integer by any of 2, 3, 5) **/
 
+    public static int notDivisibleBy235(int N){
+        int natNums = 0;
+
+        for(int i = 1; i<= N; i++){
+            if(i % 2 != 0 && i % 3 != 0 && i % 5 != 0){
+                natNums++;
+            }
+        }
+        return natNums;
+    }
 
     /** 3. Write a method called camelCaseMe so that it converts dash/underscore delimited words into camel casing.
      * The first word within the output should be capitalized only if the original word was capitalized
@@ -25,5 +47,28 @@ public class Quiz {
      "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
      "The_Stealth-Warrior" gets converted to "TheStealthWarrior" **/
 
+    public static String camelCaseMe(String word){
+        StringBuilder output = new StringBuilder();
+        boolean change = false;
+        for (int i = 0; i < word.length(); i++) {
+            char currentChar = word.charAt(i);
+            if (currentChar == '-' || currentChar == '_') {
+                change = true;
+            }
+            else {
+                if (change) {
+                    output.append(Character.toUpperCase(currentChar));
+                    change = false;
+                } else {
+                    output.append(currentChar);
+                }
+            }
+        }
 
+        String ans = output.toString();
+        if(Character.isUpperCase(ans.charAt(0))){
+            ans = Character.toUpperCase(ans.charAt(0)) + ans.substring(1);
+        }
+        return ans;
+    }
 }
